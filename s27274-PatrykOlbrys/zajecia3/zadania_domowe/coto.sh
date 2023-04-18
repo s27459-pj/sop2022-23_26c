0858d6586bf7f45cbb66c8fd2faccb95a3f4b2b6
s27274-pj <116436535+s27274-pj@users.noreply.github.com>
2023-04-18 17:15:08 +0200

    Create coto.sh

---
+++ s27274-PatrykOlbrys/zajecia3/zadania_domowe/coto.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+arg=$1
+
+if [[ $# == 0 ]]; then
+  echo "Nie podano żadnych argumentów!"
+elif [[ $# -ge 2 ]]; then
+# -ge Greater or equal
+  echo "Podaj tylko jeden argument!"
+elif [[ "$arg" =~ ^[0-9]+[0-9-]*[0-9]+$ ]]; then
+  echo "kod pocztowy"
+elif [[ "$arg" =~ ^[A-PR-UWZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+$ ]]; then
+  echo "imię"
+elif [[ "$arg" =~ [a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,} ]]; then
+  echo "email"
+fi
