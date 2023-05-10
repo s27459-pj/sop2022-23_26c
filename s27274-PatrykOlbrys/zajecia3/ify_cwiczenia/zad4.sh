5b1b21f90e4b8c9870fe3a015649475337866328
s27274-pj <116436535+s27274-pj@users.noreply.github.com>
2023-05-10 13:47:51 +0200

    Corrected zad4

--- s27274-PatrykOlbrys/zajecia3/ify_cwiczenia/zad4.sh
+++ s27274-PatrykOlbrys/zajecia3/ify_cwiczenia/zad4.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-if [[ $(ls | wc -l) > 5 ]]; then
+if [[ $(ls -p | grep -v / | wc -l) > 5 ]]; then
     echo "W bieżącym katalogu znajduje się więcej niż 5 plików"
 fi
+
+# ls -p dodaje do wszystkich katalogów na końcu znak "/"
+# A grep -v zwraca tylko linie nie zawierające znaku "/"
